24b496f8321930612655a2049c13ec2d2f9a9417
Sean Liao <seankhliao@gmail.com>
2021-06-06 23:45:53 +0200

    refactor

--- go.mod
+++ go.mod
@@ -1,3 +1,46 @@
 module go.seankhliao.com/p
 
 go 1.17
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/go-logr/logr v0.4.0
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.10.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.18.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/shirou/gopsutil v2.20.9+incompatible // indirect
+	github.com/tdewolff/minify/v2 v2.9.17 // indirect
+	github.com/yuin/goldmark v1.3.1 // indirect
+	github.com/yuin/goldmark-meta v1.0.0 // indirect
+	go.opentelemetry.io/contrib v0.20.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/host v0.20.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.20.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/runtime v0.20.0 // indirect
+	go.opentelemetry.io/otel v0.20.0 // indirect
+	go.opentelemetry.io/otel/exporters/metric/prometheus v0.20.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp v0.20.0 // indirect
+	go.opentelemetry.io/otel/metric v0.20.0 // indirect
+	go.opentelemetry.io/otel/sdk v0.20.0 // indirect
+	go.opentelemetry.io/otel/sdk/export/metric v0.20.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v0.20.0 // indirect
+	go.opentelemetry.io/otel/trace v0.20.0 // indirect
+	go.opentelemetry.io/proto/otlp v0.7.0 // indirect
+	go.seankhliao.com/w/v16 v16.0.0-20210606153222-0fc3eb88e5db
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.37.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/klog/v2 v2.9.0
+	sigs.k8s.io/yaml v1.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,60 +1,95 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"math/rand"
 	"net/http"
 	"os"
+	"os/signal"
 	"path"
 	"path/filepath"
+	"syscall"
 
+	"github.com/go-logr/logr"
 	"go.seankhliao.com/p/base20"
-
-	_ "embed"
+	"go.seankhliao.com/w/v16/render"
+	"go.seankhliao.com/w/v16/webserver"
+	"k8s.io/klog/v2/klogr"
 )
 
-//go:embed index.html
-var indexHTML []byte
-
 func main() {
-	var data, addr string
+	var data string
+	var wo webserver.Options
 	flag.StringVar(&data, "data", "data", "path to data dir")
-	flag.StringVar(&addr, "addr", "127.0.0.1:28002", "address to listen on")
+	wo.InitFlags(flag.CommandLine)
 	flag.Parse()
 
+	ctx := context.Background()
+	ctx, _ = signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+
+	l := klogr.New()
+
 	a := &App{
+		l:   l,
 		dir: data,
 	}
-	a.init()
+	err := a.init()
+	if err != nil {
+		l.Error(err, "app init")
+		os.Exit(1)
+	}
 
-	mux := http.NewServeMux()
-	mux.Handle("/index.html", http.RedirectHandler("/", http.StatusFound))
-	mux.Handle("/", a)
-	log.Fatal(http.ListenAndServe(addr, mux))
+	wo.Logger = l
+	wo.Handler = a
+
+	webserver.New(ctx, &wo).Run(ctx)
 }
 
 type App struct {
+	l   logr.Logger
 	dir string
 }
 
-func (a *App) init() {
+func (a *App) init() error {
 	err := os.MkdirAll(a.dir, 0o755)
 	if err != nil && !errors.Is(err, os.ErrExist) {
-		log.Fatal(err)
+		return err
 	}
 
-	err = os.WriteFile(filepath.Join(a.dir, "index.html"), indexHTML, 0o644)
-	if err != nil {
-		log.Fatal(err)
+	ro := &render.Options{
+		MarkdownSkip: true,
+		Data: render.PageData{
+			Title:       `paste`,
+			Description: `simple paste service`,
+			H1:          `paste`,
+			H2:          `upload`,
+			Style:       style,
+			Compact:     true,
+		},
 	}
+
+	b := bytes.NewReader([]byte(body))
+
+	fn := filepath.Join(a.dir, "index.html")
+	fout, err := os.Create(fn)
+	if err != nil {
+		return err
+	}
+
+	err = render.Render(ro, fout, b)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("ACCESS %s %s %s %s", r.Method, r.URL.Path, r.UserAgent(), r.RemoteAddr)
+	a.l.Info("access", "method", r.Method, "path", r.URL.Path, "user-agent", r.UserAgent(), "remote", r.RemoteAddr)
 	if r.Method == http.MethodPost && r.URL.Path == "/api/v0/form" {
 		a.formHandler(w, r)
 		return
@@ -129,3 +164,88 @@ func uniqDir(dir, ext string) (*os.File, error) {
 	}
 	return nil, fmt.Errorf("exceeded retries=%d", totalRetries)
 }
+
+var body = `
+<form id="form" autocomplete="off" action="/api/v0/form" enctype="multipart/form-data" method="POST"></form>
+
+<div id="text">
+<textarea
+  id="paste"
+  name="paste"
+  form="form"
+  autofocus
+  placeholder="paste something here..."
+  rows="10"
+  cols="40"
+></textarea>
+</div>
+
+<div id="buttons">
+  <label for="upload" id="uploadlabel"><em>Or upload:</em></label>
+  <input type="file" id="upload" name="upload" form="form" />
+  <input type="submit" id="submit" value="Send" form="form" />
+</div>
+
+<script>
+  const file = document.querySelector("#upload");
+  file.addEventListener("change", (e) => {
+    // Get the selected file
+    const [file] = e.target.files;
+    document.querySelector("#uploadlabel").innerHTML = "<em>Or upload:</em> ${file.name}";
+  });
+</script>
+`
+
+var style = `
+#text,
+#buttons {
+  grid-column: 1 / span 5;
+  margin: 1vmin 2vmin 0;
+}
+textarea {
+  width: 100%;
+  height: 60vh;
+  margin: 1vh 0 2vh 0;
+  font-size: 16px;
+  background-color: #303030;
+  color: #eceff1;
+  border: none;
+}
+textarea:focus {
+  outline: 3px solid #404040;
+}
+
+#upload {
+  opacity: 0;
+  width: 0.1px;
+  height: 0.1px;
+  position: absolute;
+}
+#uploadlabel {
+  float: left;
+  display: block;
+  position: relative;
+  cursor: pointer;
+  border: 2px solid #303030;
+  padding: 0.25em 1em;
+}
+#uploadlabel:hover {
+  background-color: #303030;
+  transition: 0.4s;
+}
+input[type="submit"] {
+  background-color: #000;
+  float: right;
+  font: 18px "Inconsolata", monospace;
+  font-weight: 700;
+  padding: 0.25em 1em;
+  color: #a06be0;
+  border: 2px solid #a06be0;
+}
+input[type="submit"]:focus,
+input[type="submit"]:hover {
+  outline: none;
+  background-color: #303030;
+  transition: 0.4s;
+}
+`
